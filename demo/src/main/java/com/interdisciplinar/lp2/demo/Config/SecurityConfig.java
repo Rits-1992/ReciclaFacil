96bc4ba9598704fa7fc7cab2cb66e534f3b77cd2
Rits-1992 <rits.bhr@gmail.com>
2025-12-03 16:49:20 -0300

    Alterações simples

--- demo/src/main/java/com/interdisciplinar/lp2/demo/Config/SecurityConfig.java
+++ demo/src/main/java/com/interdisciplinar/lp2/demo/Config/SecurityConfig.java
@@ -1,11 +1,13 @@
     package com.interdisciplinar.lp2.demo.Config;
     import org.springframework.context.annotation.Bean;
     import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
     import org.springframework.security.config.annotation.web.builders.HttpSecurity;
     import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
     import org.springframework.security.web.SecurityFilterChain;
     import org.springframework.web.cors.CorsConfiguration;
-
+    
     @Configuration
     @EnableWebSecurity
     public class SecurityConfig {
